9a4ba1d16c245ef4153227b5c6ee307cdb0ae9a2
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-01-07 16:24:12 +0000

    Feat(endpoints in process): created get methods

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Users, People, Planets, Vehicles, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,57 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+##SWA Endopoints
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = Users.query.all()
+    return jsonify([users.serialize() for users in users])
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    user_id = 1
+    favorites = Favorites.query.filter_by(user_id=user_id).all()
+    return jsonify([fav.serialize() for fav in favorites])
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    return jsonify([person.serialize() for person in people])
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get(people_id)
+    if not person:
+        return jsonify({"error": "Person not found"}), 404
+    return jsonify(person.serialize())
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    return jsonify([planets.serialize() for planets in planets])
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    if not planet:
+        return jsonify({"error": "Planet not found"}), 404
+    return jsonify(planet.serialize())
+
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicles.query.all()
+    return jsonify([vehicles.serialize() for vehicles in vehicles])
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_vehicle(vehicles_id):
+    vehicle = Planets.query.get(vehicles_id)
+    if not vehicle:
+        return jsonify({"error": "Vehicle not found"}), 404
+    return jsonify(vehicle.serialize())
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,95 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=True)
+    vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'), nullable=True)
+    people = db.relationship('People', backref='favorites_people', lazy=True)
+    planet = db.relationship('Planets', backref='favorites_planets', lazy=True)
+    vehicle = db.relationship('Vehicles', backref='favorites_vehicles', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Favorites %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "people_id": self.people_id,
+            "planets_id": self.planets_id,
+            "vehicles_id": self.vehicles_id
+        }
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='users', lazy=True)
+
+    def __repr__(self):
+        return '<Users %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "user_id": self.user_id
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "user_id": self.user_id
+        }
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return '<Vehicles %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "user_id": self.user_id
+        }
+    
